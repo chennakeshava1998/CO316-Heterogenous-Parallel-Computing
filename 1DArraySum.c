3ec533264c2e9c8b47aa3d0da59f0453fd45b941
Chenna Keshava B S <16co108.keshava@nitk.edu.in>
2018-07-29 19:10:43 +0530

    clock function

--- 1DArraySum.c
+++ 1DArraySum.c
@@ -1,4 +1,8 @@
+#include <cuda.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include <math.h>
 
 __global__ void add(float *A, float *B, float *C, int N)
@@ -53,27 +57,27 @@ int main()
 
     printf("\n\nCalling the kernel with %d Blocks and %d threads in each block\n", gridDim, blockDim);
 
-    double t1 = myCPUTimer();
+    double t1 = clock();
     add<<<gridDim, blockDim>>>(A, B, C, N);
     cudaDeviceSynchronize();
-    double t2 = myCPUTimer();
+    double t2 = clock();
 
-    printf("\nTime taken to add %d elements = %lf\n\n", N, t2 - t1);
+    printf("\nTime taken to add %d elements = %lf\n\n", N, (t2 - t1) / CLOCKS_PER_SECOND);
 
     // copy back to host
     printf("\n\nCalculation completed on the GPU. Fetching the answer back from the GPU's global memory\n");
     cudaMemcpy(host_C, C, N * sizeof(float), cudaMemcpyDeviceToHost);
 
     // Calculating the time required for a single thread, within a single block
-    t1 = myCPUTimer();
+    t1 = clock();
     singleThreadVecAdd<<<1, 1>>>(A, B, C, N);
     cudaDeviceSynchronize();
-    t2 = myCPUTimer();
+    t2 = clock();
 
     // copy back to host
     cudaMemcpy(host_C, C, N * sizeof(float), cudaMemcpyDeviceToHost);
 
-    printf("\nTime taken to perform %d additions with single thread and One block: %lf\n", N, t2 - t1);
+    printf("\nTime taken to perform %d additions with single thread and One block: %lf\n", N, (t2 - t1) / CLOCKS_PER_SECOND);
 
     // free the malloc'ed memory
     printf("\n\nFree'ing the malloc'ed memory on the GPU\n");
--- 2DArraySum.c
+++ 2DArraySum.c
@@ -1,4 +1,8 @@
+#include <cuda.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include <math.h>
 
 #define M 1024
@@ -64,30 +68,30 @@ int main()
     printf("\n\nCalling the kernel with %d Blocks and %d threads in each block\n", gridDim, blockDim);
 
     // timing the GPU kernel
-    double t1 = myCPUTimer();
+    double t1 = clock();
 
     add<<<gridDim, blockDim>>>(A, B, C);
     cudaDeviceSynchronize();
-    double t2 = myCPUTimer();
+    double t2 = clock();
 
     printf("\nNumber of threads per block: %d\n", blockDim);
     printf("\nDimesions of the grid: %d BY %d BY %d\n", gridDim.x, gridDim.y, gridDim.z);
-    printf("\nTime taken to add %d elements = %lf\n\n", M * N, t2 - t1);
+    printf("\nTime taken to add %d elements = %lf\n\n", M * N, (t2 - t1) / CLOCKS_PER_SECOND);
 
     // copy back to host
     printf("\n\nCalculation completed on the GPU. Fetching the answer back from the GPU's global memory\n");
     cudaMemcpy(host_C, C, M * N * sizeof(float), cudaMemcpyDeviceToHost);
 
     // Calculating the time required for a single thread, within a single block
-    t1 = myCPUTimer();
+    t1 = clock();
     singleThreadVecAdd<<<1, 1>>>(A, B, C);
     cudaDeviceSynchronize();
-    t2 = myCPUTimer();
+    t2 = clock();
 
     // copy back to host
     cudaMemcpy(host_C, C, M * N * sizeof(float), cudaMemcpyDeviceToHost);
 
-    printf("\nTime taken to perform %d additions with single thread and One block: %lf\n", M * N, t2 - t1);
+    printf("\nTime taken to perform %d additions with single thread and One block: %lf\n", M * N, (t2 - t1) / CLOCKS_PER_SECOND);
 
     // free the malloc'ed memory
     printf("\n\nFree'ing the malloc'ed memory on the GPU\n");
